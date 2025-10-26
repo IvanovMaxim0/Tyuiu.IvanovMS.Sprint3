1b61e0c70adce5a3b4f51aa6e5cc7f56df791d5d
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-10-27 04:27:57 +0500

    Add project files.

---
+++ Tyuiu.IvanovMS.Sprint3.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.IvanovMS.Sprint3.Task0.V18.Lib;
+namespace Tyuiu.IvanovMS.Sprint3.Task0.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 6;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+
+            double wait = 103.141;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.IvanovMS.Sprint3.Task0.V18/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.IvanovMS.Sprint3.Task0.V18.Lib;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выолнил: Иванов М. С. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Оператор цикла for                                                      *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #18                                                             *");
+        Console.WriteLine("* Выполнила: Иванов Максим Сергеевич | ПКТБ-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение   *");
+        Console.WriteLine("* ряда по формуле, при x = 1:                                             *");
+        Console.WriteLine("*       6                                                                 *");
+        Console.WriteLine("*       __         2      k                                               *");
+        Console.WriteLine("*  P = |  | ( ---------- )                                                *");
+        Console.WriteLine("*      |  |   cosx + 0.5                                                  *");
+        Console.WriteLine("*      k=1                                                                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* x = 1                                                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int start = 1;
+        int stop = 6;
+        int x = 1;
+        Console.WriteLine(ds.GetMultiplySeries(x, start, stop));
+    }
+}
