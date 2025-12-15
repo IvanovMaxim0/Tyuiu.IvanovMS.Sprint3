26be22bd8aefedf781cc487c5a1483407c4d5851
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-12-15 18:07:02 +0500

    Task6

---
+++ Tyuiu.IvanovMS.Sprint3.Task6.V19/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.IvanovMS.Sprint3.Task6.V19.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выолнил: Иванов М. С. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Обработка целочисленной информации                                      *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #19                                                             *");
+        Console.WriteLine("* Выполнил: Иванов Максим Сергеевич | ПКТБ-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+        Console.WriteLine("* числовому отрезку [10, 15] сумму всех делителей меньше 8                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* start = 10, stop = 15                                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int start = 10;
+        int stop = 15;
+        Console.WriteLine(ds.GetSumTheDivisors(start, stop));
+    }
+}
