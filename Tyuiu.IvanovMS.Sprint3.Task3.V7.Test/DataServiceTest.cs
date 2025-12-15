e02454f5b2e2d7cc678d1af92e0feda4df113f34
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-12-15 16:37:41 +0500

    Task3

---
+++ Tyuiu.IvanovMS.Sprint3.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.IvanovMS.Sprint3.Task3.V7.Lib;
+namespace Tyuiu.IvanovMS.Sprint3.Task3.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidReplaceCharOnNum()
+        {
+            DataService ds = new DataService();
+            string str = "gft hggt ntg";
+            char repleacable = 'g';
+            char replacement = '4';
+            string wait = "4ft h44t nt4";
+            var res = ds.ReplaceCharOnNum(str, repleacable, replacement);
+            
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.IvanovMS.Sprint3.Task3.V7/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.IvanovMS.Sprint3.Task3.V7.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выолнил: Иванов М. С. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Оператор цикла foreach                                                  *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #7                                                              *");
+        Console.WriteLine("* Выполнила: Иванов Максим Сергеевич | ПКТБ-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Используя цикл foreach заменить буквы g на цифру 4 в строке:            *");
+        Console.WriteLine("* gft hggt ntg                                                            *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string str = "gft hggt ntg";
+        char replaceable = 'g';
+        char replacement = '4';
+        Console.WriteLine(ds.ReplaceCharOnNum(str, replaceable, replacement));
+    }
+}
